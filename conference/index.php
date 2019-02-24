d5f2849765068f19cf62dd3d7d02778037729515
kungminlin <mml0816@gmail.com>
2019-02-23 21:34:39 -0800

    update speaker names

--- conference/index.php
+++ conference/index.php
@@ -4,7 +4,7 @@
         <?php include('../templates/meta_heading.php') ?>
     </head>
     <body>
-      
+
       <?php include('../templates/header.php') ?>
 
       <div class="row">
@@ -29,7 +29,7 @@ With our distinguished set of speakers and performers, we hope to provide a conf
       </div>
 
 
-      <div class="row" id="schedule">
+      <!-- <div class="row" id="schedule">
 	<div class="two columns"></div>
 	<div class="eight columns">
 	  <h2>Schedule</h2>
@@ -111,254 +111,165 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 	    </tbody>
 	  </table>
 	</div>
-      </div>
+      </div> -->
 
       <div class="row">
 	<div class="two columns"></div>
 	<div class="eight columns">
 	    <h2>Speakers</h2>
 
-	    <div class="profile row" id="noelle_hanson">
+	    <div class="profile row" id="benjamin_rubinstein">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/noelle_hanson.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Noelle Hanson</span>
+		<span class="name">Benjamin Rubinstein</span>
 		<span class="bio">
-          Noelle Hanson is a junior at LAHS. She is a contemporary and modern dancer and enjoys international travel and trying new foods from all over the world. She loves political podcasts, NPR and hopes to learn two languages before graduating college.<br /><br />
-
-		  <b>Noelle will be speaking on "Synesthesia and the Connections it Makes"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="shilpa_rao">
+      <div class="profile row" id="charles_atkins">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/shilpa_rao.jpeg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Shilpa Rao</span>
+		<span class="name">Charles Atkins</span>
 		<span class="bio">
-		  As a musician able to play the guitar, flute, and piano, Shilpa Rao is well versed in the arts. She advocates for the incorporation of Art into the educational curriculum of students, believing that nonlinear problem solving is an integral skill to being successful in the complex and multidimensional world.<br /><br />
-
-		  She also founded a non profit called STEAM Team in 2017, which aims to level the playing field in education and offer every kid the same shot at education.<br /><br />
-
-		  <b>Shilpa will be speaking on "The Oil And Water of Education (A Student's Perspective on Art in STEM)"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="hunter_liu">
+      <div class="profile row" id="noah_tesfaye">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/hunter_liu.jpeg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Hunter Liu</span>
+		<span class="name">Noah Tesfaye</span>
 		<span class="bio">
-		  Hunter is passionate about science, music, and reading. He is a strong believer in science and rational thought and also enjoys a variety of music spanning across many genres.<br /><br />
-
-		  <b>Hunter will be speaking on "Perception and Racism: the Magic in our Minds"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="angelina_lue">
+      <div class="profile row" id="richa_krishna">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/angelina_lue.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Angelina Lue</span>
+		<span class="name">Richa Krishna</span>
 		<span class="bio">
-		  After learning about the severity of the elephant poaching crisis, Angelina Lue started her own online clothing business, named Ivory Trees, to help save elephants. At the age of 13 she used her proceeds to foster a baby elephant that was shot twice in Africa. Angelina uses her free time to cook, paint watercolor, and make cards for people.<br /><br />
-
-		  <b>Angelina will be speaking on "How We Can Save the Elephants"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="sriram_ramamurthy">
+      <div class="profile row" id="kayley_gould">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/sriram_ramamurthy.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Sriram Ramamurthy</span>
+		<span class="name">Kayley Gould</span>
 		<span class="bio">
-		  Sriram Ramamurthy has been playing piano for 12 years and trombone for 7, has been fencing for 6 years, and is a self-taught home aquarist. He takes care of a variety of exotic aquatic animals, each of which requiring extreme care and difficulty. Through his talk about this hobby, he hopes to enable listeners to garner a greater appreciation for the beauty, fragility, and complexity of nature.<br /><br />
-
-		  <b>Sriram will be speaking on "Aquarium - A Life Story"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="zanny_zellers">
+      <div class="profile row" id="elise_nguyen">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/zanny_zellers.JPG" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Zanny Zellers</span>
+		<span class="name">Elise Nguyen</span>
 		<span class="bio">
-		  Zanny is a senior at Los Altos High School who enjoys reading, drawing, taking care of plants, and jumping over hurdles on the track. With #minimalism appearing on over 9 million Instagram posts worldwide, it is clear that the world is starting to favor the sparsest, simplest of places. Living in an extremely cluttered home was a proscription on how she defined herself. After many trips to Goodwill, some introspection, and a few difficult conversations, Zanny realized in order to save her relationships she would need to alter the way she saw stuff.<br /><br />
-
-		  <b>Zanny will be speaking on "Why I’m Happy to Live in a Messy House"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="jia_seow">
+      <div class="profile row" id="zach_bodner">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/jia_seow.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Jia Seow</span>
+		<span class="name">Zach Bodner</span>
 		<span class="bio">
-		  Born in Singapore and raised in the Bay Area, Jiahui (Jia) Seow is a 17-year-old published poet, neuroscience enthusiast, and aspiring publicist. She is currently a senior at LAHS. When she was 14 years old, Jia discovered poetry as an art form, which evolved into a positive outlet for her to express herself freely. One year later, Jia published Return, her first poetry collection. She has earned an Honorable Mention and Silver Key from the Scholastic Art and Writing Awards. Jia will pursue communications, neuroscience, psychology, and creative writing at Santa Clara University in the fall.<br /><br />
-
-		  <b>Jia Seow will be speaking on "How Poetry Can Help Us Move On"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="andrew_rathjen">
+      <div class="profile row" id="jerome_behar">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/andrew_rathjen.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Andrew Rathjen</span>
+		<span class="name">Jerome Behar</span>
 		<span class="bio">
-		  While in the military Andrew Rathjen reached the rank of the Staff Sergeant, US Air force with TSSI clearance, which is the top security clearance in US military. His main job was the Geospatial Intelligence Analyst. Mr. Rathjen completed his military service working on the Ground Moving Target Indication (GMTI). He currently works as an Instructional Aide at LAHS, while working toward his teaching degree<br /><br />
-
-		  <b>Mr. Rathjen will be speaking on "Pursuit of Happiness"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="amber_emily">
+      <div class="profile row" id="jill_olenka">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/amber_jackson.jpg" />
-		<img class="headshot" src="/img/headshots/emily_callahan.jpg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Amber Jackson & Emily Callahan</span>
+		<span class="name">Jill Asher and Olenka Villarreal</span>
 		<span class="bio">
-		  Amber Jackson graduated from UC Berkeley with a B.A. in Marine Science. She also has a M.A.S. in in Marine Biodiversity and Conservation from Scripps Institution of Oceanography. Emily Callahan graduated from Scripps Institution of Oceanography with a B.A. in Environmental Science and an M.A.S degree in Marine Biodiversity and Conservation. Both Ms. Jackson and Ms. Callahan currently work as oceanographers and divers for Rigs-to-Reefs. There comes a time when the useful life of an oil platform comes to an end, at least when it comes to drilling for oil, and that’s when Amber and Emily dive in. They are marine scientists on a mission to re-purpose offshore oil and gas platforms as artificial reefs around the world.<br /><br />
-
-		  <b>Ms. Jackson and Ms. Callihan will be speaking on "Rigs to Reefs: Exploring the Future of Offshore Oil and Gas Development"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="cristy_dawson">
+      <div class="profile row" id="brian_weisfeld">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/cristy_dawson.jpeg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Cristy Dawson</span>
+		<span class="name">Brian Weisfeld</span>
 		<span class="bio">
-		  What is resilience? Why is it so important to our success as people? Cristy Dawson will talk about the phenomena of bouncing back after facing obstacles. How do we access resources within us and from our community? She will share her story and how she survived adversity in my childhood to achieve success personally and professionally.<br /><br />
-
-		  <b>Ms. Dawson will be speaking on "Battling Back on the Outside Begins with Fighting Back on the Inside"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="bob_mcfarlane">
+      <div class="profile row" id="isaiah_swift">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/bob_mcfarlane.png" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Bob McFarlane</span>
+		<span class="name">Isaiah Swift</span>
 		<span class="bio">
-		  Bob McFarlane is the Founder & CEO of Bald Eagle Sports Camps which serves over 1500 campers a year. He has taught Special Ed and PE since 1995, currently at Los Altos HS. “Coach Bob” has over 25 years coaching experience from youth to collegiate, is a Certified Trainer for The Positive Coaching Alliance and holds two teaching Credentials along with a Masters Degree in Special Education.  In his 4 years as the Head Varsity Boys Basketball Coach at Los Altos High School he led the Eagles to 2 SCVAL titles ('13,'14) and the schools first ever Central Coast Section Championship (2015).<br /><br />
-
-		  <b>Mr. McFarlane will be speaking on "Winners, Losers...and the Real You"</b>
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-	    <div class="profile row" id="adam_warmoth">
+      <div class="profile row" id="paul_thiebaut">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/adam_warmoth.jpeg" />
+		<img class="headshot" src="/img/headshots/placeholder.png" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Adam Warmoth</span>
+		<span class="name">Paul Thiebaut III</span>
 		<span class="bio">
-		  Adam Warmoth graduated from LAHS in 2012 and Stanford University in 2017 with a B.A. in Engineering Physics as well as a M.S. in Mechanical Engineering. During Adam’s time at Stanford he played on the Varsity Water Polo team and captained the team during his senior year. Adam plans on doing his talk about new developments in technology that are leading us toward a future where people will use electric vertical takeoff and landing aircraft to move around cities, as well as the impact that this new technology will have on our lives.<br /><br />
-
-		  <b>Mr. Warmoth will be speaking on "The Future of Urban Air Mobility"</b>
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="julie_lythcott-haims">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/julie_lythcott-haims.jpg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Julie Lythcott-Haims</span>
-		<span class="bio">
-		  Julie Lythcott-Haims is a former Dean of Freshmen and Undergraduate Advising for Stanford University, a New York Times bestselling author, and a TED Talk Speaker. Having gotten her BA in American Studies from Stanford, a JD from Harvard Law School, and an MFA from the California College of the Arts, Julie uses her experiences to talk about colleges that tend to undeservingly go under the radar.<br /><br />
-
-		  <b>Ms. Lythcott will be speaking on "How America Made Me Loathe Myself and What I Did About it"</b>
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="caley_vahedi">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/caley_vahedi.jpg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Caley Vahedi</span>
-		<span class="bio">
-		  Caley is currently a senior at LAHS and she has been taking singing and guitar lessons since she was 9. Caley only recently started writing her own music, with her first original song completed around January of this year with her best friend Sara Bares, who goes to Pinewood.<br /><br />
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="shark_cops">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/shark_cops.jpeg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Shark Cops</span>
-		<span class="bio">
-		  The Shark Cops is a band composed of juniors Dilip Kumar, Owen Mackenzie, and Krishna Tenneti. They have been playing together for 5 years now. Dilip plays bass, Owen Mackenzie is the lead singer and guitarist, and Krishna is on the drums. <br /><br />
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="advanced_jazz">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/advanced_jazz.jpg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Advanced Jazz</span>
-		<span class="bio">
-		  Advanced Jazz Dance is an audition-only class that meets 4th period.  Students in the class have a wide range of dance experience, some having focused on ballet and others on jazz and contemporary. The class curriculum places an emphasis on the creation of original choreography. The dance they're performing today is the result of a class project: each small group of dancers choreographed one section of the dance and then taught the other students. These dancers, and the students in the Jazz Dance classes, will be performing next quarter in the Spring Jazz Dance Show on May 17 and 18. We hope to see you there!<br /><br />
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="yalda_elena_cathy">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/yalda_elena_cathy.jpeg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Yalda Khodadad, Elena Mujica, & Cathy Wang</span>
-		<span class="bio">
-		  Elena’s poem is called Identity Crisis and it's about her identity as a white Latina and how that has clashed with societal expectations of what a Latina should look like. Cathy will be performing a poem examining a feeling of displacement in the world as an immigrant titled Allegiance. Yalda is performing her poem Rosemilk, which she decided to write after seeing an Iranian talk show in which the hosts legitimized domestic abuse.<br /><br />
-		</span>
-	      </div>
-	    </div>
-
-	    <div class="profile row" id="ryan_cardona">
-	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/ryan_cardona.jpg" />
-	      </div>
-	      <div class="ten columns">
-		<span class="name">Ryan Cardona</span>
-		<span class="bio">
-		  Ryan Cardona is a top tier memer, who spends a lot of time playing video games, doing tech related things and oh ya and yo-yoing. After learning that there is more to the yoyo than just going up and down and doing walk the dog, he wanted to get more into the Spobby (sport/hobby) of yo-yoing, learning four out of the five styles. Preferring to mostly listen to EDM has shaped his freestyle and trick pool.<br /><br />
+          <!-- Speaker Bio Here -->
+		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
