12ba7ebe2cb99b3f7a7f75de64822ad2f25a6912
Leo <mml0816@gmail.com>
2019-03-11 10:11:08 -0700

    update speaker bio

--- conference/index.php
+++ conference/index.php
@@ -14,10 +14,10 @@
 
 	  <div class="profile row">
 	    <div class="four columns">
-	      <img class="headshot" src="/img/theme_logo.png" />
+	      <img class="headshot" src="/img/theme_logo.jpg" />
 	    </div>
 	    <div class="eight columns">
-	      <span class="name">Ideas creaTED</span>
+	      <span class="name">creaTED</span>
 	      <span class="bio">
 		TEDxLAHS aims to share ideas that serve to benefit the community, whether it be through raising awareness of social issues, educating others on unfamiliar topics, or by giving performances that allow the audience to appreciate what their community has to offer. At TEDxLAHS, we work to provide an event that enriches the lives of the audience. We share ideas that challenge common thought, ideas that enlighten our community, and ideas that improve our world. We share what we have <b>creaTED</b>.<br /><br />
 
@@ -44,7 +44,7 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 	      <tr>
 		<td>Period 1</td>
 		<td>
-			<a href="#charles_atkins">Charles AtkinsNoelle Hanson</a><br>
+			<a href="#charles_atkins">Charles Atkins</a><br>
 			<a href="#isaiah_swift">Isaiah Swift</a><br>
 		</td>
 	      </tr>
@@ -100,7 +100,7 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 		<td>Period 6</td>
 		<td>
 			<a href="#richa_krishna">Richa Krishna</a><br>
-			<a href="#zach_bodner">Zach Bodner</a>
+			<a href="#zack_bodner">Zack Bodner</a>
 		</td>
 	      </tr>
 	    </tbody>
@@ -115,12 +115,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
 	    <div class="profile row" id="benjamin_rubinstein">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Ben Rubinstein.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Benjamin Rubinstein</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Ben Rubinstein is a Junior in High school who allows his passion for quirky things to drive his decision-making process and pursues his passions to the extreme, whether it means creating a language or building a rocket.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -128,12 +128,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="charles_atkins">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Charles Atkins.png" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Charles Atkins</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Charles Atkins is a sophomore at Los Altos High School. He is passionate about the humanities, especially philosophy, which he believes can teach us what it means to be human and how we can best live. He has his own blog, Neologikon, where he writes about his interests, in hopes of inspiring others to ask questions and think for themselves. Charles also runs for the Los Altos track and field varsity team.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -141,12 +141,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="noah_tesfaye">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Noah Tesfaye.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Noah Tesfaye</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Noah Tesfaye is a senior at Los Altos and web managing editor for the Talon. He is extremely passionate about politics and constitutional law, and he hopes to major in political science in college. In his free time, he enjoys reading the news, listening to podcasts on criminal justice, and eating great food with friends. His favorite books he’s recently read are <em>Just Mercy</em> by Brian Stevenson and <em>If Beale Street Could Talk</em> by James Baldwin.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -154,12 +154,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="richa_krishna">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Richa Krishna.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Richa Krishna</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Richa Krishna is currently a senior at Los Altos High School. She began ballet at age 4, which developed into an intense appreciation for the art form through her pre-professional training. Richa has gotten very involved in making, entrepreneurship, and design thinking in recent years and is currently working on developing her business, <a href="https://pulsewearables.com/">Pulse Wearables</a>, looking to study product design in college.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -167,12 +167,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="kayley_gould">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Kayley Gould.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Kayley Gould</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Kayley Gould is a senior at Los Altos High School. She enjoys swimming, baking, and warm weather. Kayley loves volunteering, and with more experience, has become interested in the white savior complex. She has been researching the topic with an emphasis on orphanage tourism for her Senior Project since November. She hopes to share with you what she has learned about the different aspects of the complex, as well as what she thinks we as high schoolers can do to address the negative effects of voluntourism.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -180,25 +180,27 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="elise_nguyen">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Elise Nguyen.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Elise Nguyen</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Elise Nguyen is a senior at Los Altos High School. She’s the embodiment of a typical teenager - she loves dogs, shopping, and traveling. She hates homework and waking up early. She’s also Malaysian born, a former figure skater, a daydreamer, and a 17-year-old girl, but above all, she’s a human being. After losing somebody close to her, she wants to reach out to her community and address something that every human will experience - grief and heartbreak - and tell others what we can do for ourselves and for others when bad things happen.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
 	    </div>
 
-      <div class="profile row" id="zach_bodner">
+      <div class="profile row" id="zack_bodner">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Zach Bodner.jpg" />
 	      </div>
 	      <div class="ten columns">
-		<span class="name">Zach Bodner</span>
+		<span class="name">zack Bodner</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+      Zack Bodner joined the OFJCC as the CEO in August of 2013. Under Zack’s leadership, the JCC completed construction of the campus, transforming it into a community-gathering space where it is filled with people of all ages and backgrounds at all hours of the day. Before joining the JCC, Zack served for 14 years as the Pacific Northwest Regional Director of the American Israel Public Affairs Committee (AIPAC), where he oversaw a period of unprecedented growth in community outreach, political advocacy and fundraising.
+      <br>Zack is an accomplished speaker and writes for publications throughout the U.S. and Israel. Zack holds degrees from  Yale and Claremont Graduate University. He has also completed Stanford Business School’s program for Executives in Non-Profit Leadership.
+
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -206,12 +208,16 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="jerome_behar">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Jerry Behar.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Jerome Behar</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+      Jerry Behar co-founded in 2018 and is the Chairman of Grand River Solutions which provides Title IX and Clery (safety) compliance and reporting solutions and services to colleges and universities. Jerry is the founder and CEO of Nissim Capital and is on the board of directors of Click Diagnostics.
+      <br>Jerry was previously the founding CEO and visionary of Workiva (NYSE: WK), and was managing director through March 2013. Workiva created Wdesk, a cloud-based platform for enterprises to collect, manage, report and analyze business data in real time. Wdesk includes a sophisticated productivity suite for business data collaboration and reporting that is used by thousands of corporations, including more than 75% of the Fortune 500.
+      <br>Prior to Workiva, Jerry was the founder and CEO of Financial Intelligence (#33 on 2008 Inc 500 list of United States’ fastest growing private companies). He has additional executive experience from previous positions held at MS2, Inc. (CFO), International Risk Control (Co-Founder), Engineering Animation (CFO), Woodside Hotels (VP, Finance) and Matrix Pharmaceutical (Controller). Jerry also served on the board / audit committee of Portal Software prior to the acquisition by Oracle in 2006.
+      <br>Jerry is a C.P.A. (inactive). He earned his B.A. from Michigan State University and his M.B.A. from Stanford University.
+
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -219,12 +225,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="jill_olenka">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Jill Asher.png" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Jill Asher and Olenka Villarreal</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Jill Asher is part of the founding team of Magical Bridge Playground in Palo Alto and co-founder of Magical Bridge Foundation. A seasoned public relations and marketing executive, Jill left a successful high tech career to run the Magical Bridge Foundation and lead administration, community engagement, development, public relations and marketing. Jill actively engages with communities to promote inclusive play and kindness through a full spectrum of communications and fundraising programs. She is instrumental in promoting the values and principles of Magical Bridge to the media and the global community. She holds degrees in Economics and English from Rutgers University.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -232,12 +238,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="brian_weisfeld">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Brian Weisfeld.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Brian Weisfeld</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Brian Weisfeld has been building businesses his entire life. As a kid, he bought gummy bears and hired his friends to sell them. As a teen, he sorted baseball cards, babysat, and sold mixtapes. As an adult, he helped build well-known, billion-dollar companies including IMAX Corporation and Coupons.com. Brian is the Founder and Chief Squad Officer of The Startup Squad, an initiative dedicated to empowering girls with an entrepreneurial mindset to help them reach their potential, whatever their passions. His first book, <em>The Startup Squad</em>, will be released by Macmillan on May 7, 2019. Brian lives in Silicon Valley with his wife and two daughters.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -245,12 +251,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="isaiah_swift">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Isaiah Swift.jpg" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Isaiah Swift</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+      Isaiah Swift was born and raised in Norfolk, VA and graduated from Maury High School as an AP Scholar with distinction. During his senior year, He was dual enrolled at Tidewater Community College, where He received six transferrable credits towards His degree. At Maury High School, Isaiah was the Branch Manager of Commodore Branch, a subsidiary of Port Alliance Credit Union, and He was a member of Future Business Leaders of America. He was awarded a full ride academic scholarship to attend Norfolk State University, where He is pursuing a degree in Business Finance. In addition, this rising junior currently works as an intern for Fresh Prints- a clothing design company that offers consumers, deals for purchasing customized apparel in bulk, His role consists of promoting the brand and overseeing the details of all orders. During his first year, Isaiah sold nearly $25,000 worth of merchandise. Recently, Isaiah published a book called “Unlock Your Greatness” 8 Ways to be Successful in High School and Beyond. In the book, He teaches students how they can be better prepared for the real world after they graduate, and provides them with valuable insight as to how they can find volunteer, internship and scholarship opportunities in their community. In His spare time, Isaiah enjoys traveling, spending time with His family and serving as a volunteer for Step Up for Down Syndrome, Ronald McDonald House Charities of Norfolk and the Roc Solid Foundation.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
@@ -258,12 +264,12 @@ With our distinguished set of speakers and performers, we hope to provide a conf
 
       <div class="profile row" id="paul_thiebaut">
 	      <div class="two columns">
-		<img class="headshot" src="/img/headshots/placeholder.png" />
+		<img class="headshot" src="/img/headshots/Paul Thiebaut III.png" />
 	      </div>
 	      <div class="ten columns">
 		<span class="name">Paul Thiebaut III</span>
 		<span class="bio">
-          <!-- Speaker Bio Here -->
+          Paul Thiebaut III is a human potential educator, scientist, and entrepreneur whose work for the past 15 years has focused on leveraging interest and intrinsic motivation to enhance student learning, performance, achievement, and well-being. Thiebaut has worked with students of all types, including special and general needs, English-learners, and preschool to PhD level. Thiebaut’s achievements include overcoming poverty by discovering his own interests and motivations, inventing the educational approach, Thiebaut Method, founding Thiebaut Method Tutoring in 2007, and founding the early education tutoring nonprofit, 10 Books A Home, in 2009. Thiebaut has a B.A. in economics and minor in business from SJSU. Thiebaut’s vision is to redesign education to cultivate interest and intrinsic motivation.
 		  <b><!-- Speaker Content Here --></b>
 		</span>
 	      </div>
--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 
                     <b>Tickets</b><br /><br />
 
-                    If you are interested in attending any of the class periods, tickets will be available at brunch and lunch in the quad from Wednesday, March 20th to Monday, March 25th and in the library at any time. You will need to get this ticket signed by any teacher of a class that you would miss to attend TEDxLAHS. You can also download the form <a href="https://docs.google.com/document/d/1m2HC3_BYZBlXNTljWyEQEzw0REOz44GzwRRD1-6jatw/edit?usp=sharing">here</a>.
+                    If you are interested in attending any of the class periods, tickets will be available at brunch and lunch in the quad and in the library at any time from Wednesday, March 20th to Monday, March 25th. You will need to get this ticket signed by any teacher of a class that you would miss to attend TEDxLAHS. You can also download the form <a href="https://docs.google.com/document/d/1m2HC3_BYZBlXNTljWyEQEzw0REOz44GzwRRD1-6jatw/edit?usp=sharing">here</a>.
 
       	      <!-- <a href="https://www.eventbrite.com/e/tedxgunnhighschool-2018-tickets-39669027128">
       		<button style="margin-bottom: 15px;">Register Now</button> -->
