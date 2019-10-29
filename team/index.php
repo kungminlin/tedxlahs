8bd8a7fae24e8713eed1f4bedc439ffa0ea339bb
Leo Lin <mml0816@gmail.com>
2019-10-29 15:23:38 -0700

    update officers

--- team/index.php
+++ team/index.php
@@ -13,44 +13,66 @@
 	    <h2>Meet the Team</h2>
 	    <div class="row">
 	      <div class="six columns">
-		  <h3>Madison Woo</h3>
-		  <h4>President</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/madison.JPG">
-		  <p>Madison Woo is a senior at Los Altos High School. She has been a part of the school newspaper, The Talon, for three years and is the Arts and Culture editor. She also is the school’s literary magazine co editor in chief and is part of E3 Philanthropy group. This is her third year being part of the TEDx team.</p>
+    		  <h3>Sara Pezzini</h3>
+    		  <h4>President</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/sara.JPG">
+    		  <p>Sara Pezzini is a senior at Los Altos High School. Last year, she served as the President of Event Planning, and this year is taking on the position of Co-President. She is a member of the Make a Birthday Wish Club, and plans on majoring in biology next year in college. This will be her third year on the TEDx team.</p>
 	      </div>
 	      <div class="six columns">
-		  <h3>Janie Dent</h3>
-		  <h4>President of Promotion</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/janie.jpg">
-		  <p>Janie Dent is a junior at Los Altos High School. She has danced competitively with the San Juan Sensations Dance Team for 7 years, and is captain of the LAHS Dance Team. She is also a writer for the school newspaper, The Talon, a member of National Honor Society, and a reading tutor for children in the Kids Read program at the Mountain View Library. This is her second year on the TEDx team.</p>
+    		  <h3>Janie Dent</h3>
+    		  <h4>President of Promotion</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/janie.jpg">
+    		  <p>Janie is a senior at Los Altos High School. She has been a part of a competitive dance team for 8 years and is the captain of the LAHS Dance Team. She is also the News Editor for The Talon, the school newspaper, and a reading tutor at Castro Elementary School. This is Janie's third year on the TEDx team.</p>
+        </div>
+	    </div>
+	    <div class="row">
+	      <div class="six columns">
+    		  <h3>Max Silver</h3>
+    		  <h4>President of Event Planning</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/sara.jpg">
+  	      <p>Max Silver is a senior at Los Altos High School. He is a part of the rock climbing club, as well as several other outdoors and environmental groups. He has also been involved in research at NASA's Ames Research Center for the past year. This is his second year on the TEDx team.</p>
+	      </div>
+	      <div class="six columns">
+    		  <h3>Boris Palant</h3>
+    		  <h4>President of Speaker Management</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/boris.jpg">
+          <p>Boris Palant is a senior at Los Altos High School. He has played water polo competitively for the last 6 years and has played on the Los Altos varsity team since he was a freshman serving as a captain Junior and Senior year. Last year, Boris served as the President of Speaker Management on the TEDx team, and this is his third year overall on the team.</p>
 	      </div>
 	    </div>
 	    <div class="row">
 	      <div class="six columns">
-		  <h3>Sara Pezzini</h3>
-		  <h4>President of Event Planning</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/sara.jpg">
-	      <p>Sara Pezzini is a junior at Los Altos High School. This is her 9th year dancing, and she has participated in the dance team and the advanced jazz class at LAHS. She has also taken part in clubs such as One Dollar For Life and Make a Birthday Wish. This is her second year on the TEDx team.</p>
+    		  <h3>Tripp Crissman</h3>
+    		  <h4>President of Finance</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/megan.jpg">
+          <p>Tripp Crissman is a senior at Los Altos High School. He has played competitive lacrosse for the past 11 years and is the captain of the boys varsity lacrosse team. He is also the Fundraising Coordinator and Treasurer for SCL. This is Tripp’s third year on the TEDx team.</p>
 	      </div>
-	      <div class="six columns">
-		  <h3>Boris Palant</h3>
-		  <h4>President of Speaker Management</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/boris.jpg">
-	      <p>Boris Palant is a junior at Los Altos High School. He has played water polo competitively for the last 5 years and has played on the Los Altos varsity team since he was a freshman. Last year, Boris served as the Vice President of Speaker Management on the TEDx team, and this is his second year overall on the team.</p>
+        <div class="six columns">
+    		  <h3>Megan Liu</h3>
+    		  <h4>President of Finance</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/megan.jpg">
+          <p>Megan Liu is a junior at Los Altos High School. She is an editor for Aerie, the school yearbook, and is a captain on the MVLA Speech and Debate team. She is also an avid dancer, having danced for 12 years at Pacific Ballet Academy and is an Ambassador in her Girl Scout Troop. This is her second year on the TEDx team.</p>
 	      </div>
 	    </div>
-	    <div class="row">
-	      <div class="six columns">
-		  <h3>Megan Liu</h3>
-		  <h4>President of Finance</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/megan.jpg">
-	      <p>Megan Liu is a junior at Los Altos High School. She is an editor for Aerie, the school yearbook, and is a captain on the MVLA Speech and Debate team. She is also an avid dancer, having danced for 12 years at Pacific Ballet Academy and is an Ambassador in her Girl Scout Troop. This is her second year on the TEDx team. </p>
+      <div class="row">
+        <div class="six columns">
+    		  <h3>Greg Corn</h3>
+    		  <h4>President of Communications</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/greg.jpg">
+    		  <p>Greg Corn is a senior at Los Altos High School. This is his third year on the TEDx team. He is a videographer for The Talon, Los Altos High School's Student Newspaper, and president of the Pretzel Appreciation Club.</p>
 	      </div>
 	      <div class="six columns">
-		  <h3>Greg Corn</h3>
-		  <h4>President of Communications</h4>
-		  <img style="margin-top:20px" class="headshot" src="/img/team/greg.jpg">
-		  <p>Greg Corn is a junior at Los Altos High School. This is his second year on the TEDx team. Greg does the motion graphics for the New Media Lit morning announcements, and runs his own <a href="https://www.youtube.com/gregcorn1">YouTube Channel</a>.</p>
+    		  <h3>Kyle Bistrain</h3>
+    		  <h4>President of Communications</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/greg.jpg">
+    		  <p>Kyle Bistrain is a senior at Los Altos High School. This is his third year on TEDx. He has played water polo for the last 7 years and plays on the Los Altos varsity team. He is also a part of the Varsity Lacrosse team. He is a President of Speaker Management.</p>
+	      </div>
+	    </div>
+      <div class="row">
+	      <div class="six columns">
+    		  <h3>Jackson Camp </h3>
+    		  <h4>President of Communications</h4>
+    		  <img style="margin-top:20px" class="headshot" src="/img/team/greg.jpg">
+    		  <p>Jackson “Camper” Camp is a senior and he plays varsity lacrosse here at the high school. This is his third year on TEDx and this year he is the Co-President of Speaker Management.</p>
 	      </div>
 	    </div>
 
